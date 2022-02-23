b439e6388bea96480084dc23f6256e8eedf33e50
pybin94 <73331322+pybin94@users.noreply.github.com>
2022-02-23 18:26:50 +0900

    Add files via upload

---
+++ upload_file.php
@@ -0,0 +1,63 @@
+
+<form method="post" action="./upload_file_submit.php" method="post" enctype="multipart/form-data">
+
+    <p>파일1</p>
+    <input type="file" name="img_1" required>
+    <p>파일2</p>
+    <input type="file" name="img_2">
+
+    <p>이미지 파일 첨부(JPG,GIF,BMP,PNG), 최대 업로드 가능 용량 2MB</p>
+
+    <button id="saveBtn" type="submit">저장</button>
+
+</form>
+
+<script>
+    const exchangeBtn = document.querySelector("#exchangeBtn")
+    exchangeBtn.addEventListener("click", function(){
+
+        const img1 = document.querySelector("#img1")
+        const img2 = document.querySelector("#img2")
+        const reg = /(.*?)\.(jpg|jpeg|png|gif|bmp|JPG|JPEG|PNG|GIF|BMP)$/;
+        const maxSize  = 2 * 1024 * 1024
+
+        if(img1.files[0].size > maxSize){
+            alert("해당 파일은 2MB를 초과합니다.")
+            img1.focus()
+            img1.value = null;
+            return false;
+        }
+        
+        if( !img1.value.match(reg)){
+            
+            alert("해당 파일은 이미지 파일이 아닙니다.");
+            img1.focus()
+            img1.value = null;
+            return false;
+            
+        }
+
+        if(imgBank.files[0].size > maxSize){
+            alert("해당 파일은 2MB를 초과합니다.")
+            img2.focus()
+            img2.value = null;
+            return false;
+        }
+        
+        if ( !imgBank.value.match(reg) ){
+
+            alert("해당 파일은 이미지 파일이 아닙니다.");
+            img2.focus()
+            img2.value = null;
+            return false;
+
+        } 
+        alert("저장이 완료됐습니다.")
+        return true;
+    })
+</script>
+
+
+
+
+
---
+++ upload_file_submit.php
@@ -0,0 +1,24 @@
+<?php
+
+$name_1 = $_FILES['img_1']["name"];
+$tmp_1 = $_FILES['img_1']["tmp_name"];
+$name_2 = $_FILES['img_2']["name"];
+$tmp_2 = $_FILES['img_2']["tmp_name"];
+
+// 특정 조건에 맞지 않은 유저가 이 페이지 접근했을때
+// ex 파일 정보가 없을 때 (업로드 안 했을 때)
+
+if(!$name_1) {
+
+    header( 'Location: /upload_file.php' );
+    exit;
+
+}
+
+move_uploaded_file($tmp_id, "../../data/file/exchange/".$name_1);
+move_uploaded_file($tmp_bank, "../../data/file/exchange/".$name_2);
+
+sql_query("INSERT INTO `[table]` (`[column1]`, `[column2]`, `[...]`) VALUES ('{$name_1}', '{$name_2}', '[...]');");
+
+// 저장 완료되면 이전 페이지 이동
+header( 'Location: /upload_file.php' );
